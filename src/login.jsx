53698771f81f52c03b710a91848620614417e008
DVSY7 <gbytkdduf1004@naver.com>
2025-11-23 02:58:25 +0900

    로그인 예외처리

--- src/login.jsx
+++ src/login.jsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Loing = () => {
+  // 리액트에서 페이지를 이동하기 위해 사용하는 함수
+  const navigate = useNavigate();
+
   const [userInfo, setUserInfo] = useState({
     userID: '',
     userPassword: '',
@@ -12,8 +16,27 @@ export const Loing = () => {
   const handleSubmit = async () => {
     // REST API는 http요청의 규약을 지킨 서버요청을 말한다.
     // RESTfull API 요청이라는게 있는데 조금더 엄격하게 http요청을 지키는 API요청을 말해.
-    const result = await axios.post(`http://localhost:5000/api/login`, userInfo);
+    const result = await axios.post(
+      `http://localhost:5000/api/login`,
+      userInfo
+    );
+
+    // 예외처리
     console.log(result.data);
+    if (result.data.id_check === false) {
+      alert(result.data.msg);
+    } else if (result.data.pw_check === false) {
+      alert(result.data.msg);
+    } else {
+      // 에외처리를 통과하면 홈으로 이동
+
+      // react 방식
+      localStorage.setItem('token', result.data.token);
+      navigate('/home');
+
+      // // 기존방식
+      // window.location.href = '/home';
+    }
   };
 
   const handleChangeInput = (e) => {
@@ -22,19 +45,17 @@ export const Loing = () => {
     console.log(userInfo);
   };
 
-  const handleClick = async () => {
-    await axios.get('http://localhost:5000/api/text');
-  };
-
   return (
     <>
-      <div className={`flex flex-col`}>
+      <div
+        className={`flex flex-col justify-center items-center w-[100vw] h-[100vh]`}
+      >
         <div>
           <label>user ID:</label>
           <input
             name={`userID`}
             onChange={handleChangeInput}
-            className={`ml-2 border-[1px] border-black`}
+            className={`ml-16 border-[1px] border-black`}
             type={`text`}
             placeholder={`아이디 입력`}
           ></input>
@@ -49,14 +70,13 @@ export const Loing = () => {
             placeholder={`비밀번호 입력`}
           ></input>
         </div>
+        <button
+          onClick={handleSubmit}
+          className={`ml-24 mt-4 w-20 h-10 bg-blue-500 rounded-xl text-white font-bold hover:bg-gray-400`}
+        >
+          로그인
+        </button>
       </div>
-      <button
-        onClick={handleSubmit}
-        className={`ml-24 mt-4 w-20 h-10 bg-blue-500 rounded-xl text-white font-bold hover:bg-gray-400`}
-      >
-        전송하기
-      </button>
-      <button onClick={handleClick}>클릭하세요</button>
     </>
   );
 };
